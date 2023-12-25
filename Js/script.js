dc424137499244e569af2a213a5b2d05475b8f4a
Naim <143653983+naim3450@users.noreply.github.com>
2023-12-25 07:38:47 -0800

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+let ssc_batch = ["naim", "saim", "sakib", "roney", "arman", "azim", "mehedi", "sarbin"]
+// console.log(ssc_batch.length)
+
+for(i=0; i<=ssc_batch.length; i++){
+    console.log(ssc_batch[i])
+
+}
+
+
+
+
+
